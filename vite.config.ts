7484f2dd4d259d5fb6ae59de829502ec754090cb
gaalves09 <142449595+gaalves09@users.noreply.github.com>
2025-10-15 11:55:51 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
-{
-  import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [react()],
+  // build sem plugin react; TS já está com "jsx": "react-jsx" no tsconfig
 })
+
